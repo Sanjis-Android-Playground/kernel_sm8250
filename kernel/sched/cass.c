diphons/D8G_Kernel_oxygen

sched/cass: Select lower orig capacity cpu when util is low
Generally, CPUs with smaller orig capacity have better energy consumption.
When util is lower than a certain value, select a CPU with lower orig capacity
to save energy.

kernel/sched/cass.c
@@ -32,6 +32,10 @@ struct cass_cpu_cand {
 	unsigned long util;
 };
 
+unsigned int sched_util_threshold[CPU_NR] = {
+	646, 646, 646, 646, 500, 500, 500, 404
+};
+
 static __always_inline
 unsigned long cass_cpu_util(int cpu, bool sync)
 {
@@ -58,6 +62,12 @@ bool cass_cpu_better(const struct cass_cpu_cand *a,
 #define cass_cmp(a, b) ({ res = (a) - (b); })
 #define cass_eq(a, b) ({ res = (a) == (b); })
 	long res;
+
+	/* Prefer the CPU with lower orig capacity when util is low */
+	if((a->util <= sched_util_threshold[a->cpu] &&
+	    b->util <= sched_util_threshold[b->cpu]) &&
+	    cass_cmp(capacity_orig_of(b->cpu), capacity_orig_of(a->cpu)))
+                goto done;
 
 	/* Prefer the CPU with lower relative utilization */
 	if (cass_cmp(b->util, a->util))

Sanjis-Android-Playground/kernel_sm8250

Change-Id: Ibbb14a250130f41b3c76d3338635ff3b67997215

kernel/sched/cass.c
@@ -37,6 +37,10 @@ struct cass_cpu_cand {
 	unsigned long util;
 };
 
+unsigned int sched_util_threshold[NR_CPUS] = {
+	646, 646, 646, 646, 500, 500, 500, 404
+};
+
 static __always_inline
 void cass_cpu_util(struct cass_cpu_cand *c, int this_cpu, bool sync)
 {
@@ -111,6 +115,12 @@ bool cass_cpu_better(const struct cass_cpu_cand *a,
 	    cass_cmp(b->eff_util * SCHED_CAPACITY_SCALE / b->cap_max,
 		     a->eff_util * SCHED_CAPACITY_SCALE / a->cap_max))
 		goto done;
+	
+	/* Prefer the CPU with lower orig capacity when util is low */
+	if((a->util <= sched_util_threshold[a->cpu] &&
+	    b->util <= sched_util_threshold[b->cpu]) &&
+	    cass_cmp(capacity_orig_of(b->cpu), capacity_orig_of(a->cpu)))
+                goto done;
 
 	/* Prefer the CPU that fits the task */
 	if (cass_cmp(fits_capacity(p_util, a->cap_max),